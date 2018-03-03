d06e3fa92bd07481b7e62f21ca2beb4a8226ab2d
Thiam Hock Ng <nthock@gmail.com>
2018-03-03 16:16:09 +0800

    WIP: Add UI for login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { LoginContainer } from './bundles/session/containers';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={LoginContainer} />
+        <Route component={() => <p>Page Not Found</p>} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/bundles/session/containers/LoginContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoginContainer = ({ history }) => (
+  <div className="login-page">
+    <h1 className="title">Log in</h1>
+    <p>hey hey</p>
+  </div>
+);
+
+export default LoginContainer;
